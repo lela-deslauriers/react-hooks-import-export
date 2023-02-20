cb5d004b95b9c54b97315a99374d5358ef8a4241
Lela Deslauriers <lela.deslauriers@gmail.com>
2023-02-19 21:34:29 -0700

    commit tree 7064bfd1417d03f003962494076489f1c145c1e1

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees as parkTrees, wildlife as parkWildlife } from "./parks/RockyMountain";
 
-function ColoradoStateParks() {
+console.log(parkTrees);
+// => "Aspen and Pine"
+
+parkWildlife();
+// => "Elk, Bighorn Sheep, Moose"
+
+
+export default function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>;
+      <MesaVerde />
+    </div>
+  );
 }
